2effa1a3e1426fef80b1af136056012c65d40c0d
laadiim <112978764+laadiim@users.noreply.github.com>
2024-08-30 10:49:36 +0200

    user class

--- src/main/java/com/Contacts/Contacts/model/User.java
+++ src/main/java/com/Contacts/Contacts/model/User.java
@@ -1,12 +1,18 @@
 package com.Contacts.Contacts.model;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.UuidGenerator;
 
 @Entity
 @Table(name = "users")
 public class User {
-
+	@Id
+	@UuidGenerator
+	@Column(name = "id", unique = true, updatable = false)
 	private String userID;
 	private String name;
 	private String email;
@@ -14,5 +20,86 @@ public class User {
 	private String phoneNumber;
 	private String password;
 
+
+	private String photoURL;
+
 	private String[] contacts;
+
+	public User() {
+    }
+
+	public User(String userID, String name, String email, String address, String phoneNumber, String password, String photoURL, String[] contacts) {
+		this.userID = userID;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.password = password;
+        this.photoURL = photoURL;
+        this.contacts = contacts;
+	}
+
+	public String getPhotoURL() {
+		return photoURL;
+	}
+
+	public void setPhotoURL(String photoURL) {
+		this.photoURL = photoURL;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String[] getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(String[] contacts) {
+		this.contacts = contacts;
+	}
 }
